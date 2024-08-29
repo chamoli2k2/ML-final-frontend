e008c347941f06533b780af9505b9f17a651b921
Chamoli <godchamoli@gmail.com>
2024-08-29 22:52:30 +0530

    chore: Update Vite configuration to add proxy for backend API

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react-swc';
 
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://192.168.170.31:6969', // Replace with your backend URL
+        changeOrigin: true,
+        secure: false, // If you're using a self-signed certificate, set this to false
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+});
