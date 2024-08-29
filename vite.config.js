9941fa501de9d2ab6f9b6a0c87bf2a23744df773
Chamoli <godchamoli@gmail.com>
2024-08-29 23:03:24 +0530

    chore: Remove unnecessary proxy configuration in Vite config

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://192.168.170.31:6969', // Your backend URL
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '/api'), // Keep the `/api` prefix
-      },
-    },
-  },
+  plugins: [react()]
 });
